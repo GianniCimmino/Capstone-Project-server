29b45a2249e42bac052808f907905bdd426547bc
Gianni <giannicimmino1996@gmail.com>
2024-01-15 19:38:22 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import express from "express";
+import apiRouter from "./apiRouter.js";
 
 const server = express();
 
 const port = 5000;
 
-server.get("/test", (req, res) => {
-  res.json({ message: "Hello, world!" });
-});
+server.use("/api", apiRouter);
 
 server.listen(port, () => {
   console.log("Server listening to port: " + port);
